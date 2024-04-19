1683cc01aef35f38b2ac6b489bd3786765196333
ddindria9 <146157759+ddindria9@users.noreply.github.com>
2024-04-19 23:49:49 +0700

    Update delete.php

--- CRUD/delete.php
+++ CRUD/delete.php
@@ -2,13 +2,13 @@
 require_once 'database.php';
 require_once 'contacts.php';
 
-if (isset($_POST['Id'])) {
-    $Id = $_POST['Id'];
-    $result = Contact::delete($Id);
+if (isset($_POST['ID'])) {
+    $Id = $_POST['ID'];
+    $result = Contact::delete($ID);
     if ($result) {
-        echo "Data with ID = $Id deleted successfully.";
+        echo "Data with ID = $ID deleted successfully.";
     } else {
-        echo "Failed to delete data with ID = $Id.";
+        echo "Failed to delete data with ID = $ID.";
     }
 } else {
     echo "ID parameter is missing.";
